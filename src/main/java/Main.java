4e9ce5298f545d8dae9d0da4dfe89deb18cf4d08
Flevertyapp <Flevertyap@gmail.com>
2022-03-28 23:42:28 +0300

    Поиграл со строками)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        
+        String a1 = "hello ";
+        String a2 = "world";
+        String a3 = a1 + a2;
+        System.out.println(a3);
+        String b1 = "Число 10: ";
+        int b2 = 10;
+        String b3 = b1 + b2;
+        System.out.println(b3);
+        String c1 = "String";
+        String c2 = "[" + c1 + "] = " + 1;
+        System.out.println(c2);
+        String c3 = "Десять = " + (5 + 5);
+        System.out.println(c3);
+        int n=200; //преобразование примитивов в строку
+        c3=String.valueOf(n);
+        System.out.println(c3);
+
+
     }
 }
